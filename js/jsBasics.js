2567114261cc0a47ac8a0764a107805a7d54a5dd
Alejandro Nieto Luque <aleexnl@MacBook-Pro.local>
2020-06-01 15:49:58 +0200

    Operator examples.

--- js/jsBasics.js
+++ js/jsBasics.js
@@ -13,3 +13,24 @@ let string = "Hello World!"; // String variable
 
 let isOn = false; // Boolean variable
 isOn = true; // Change Boolean variable
+
+// OPERATORS
+let num1 = 10; // Number variable
+let num2 = 5; // Number variable
+let name = "Alejandro"; // String variable
+
+console.log(num1 + num2); // Sum Result: 15
+console.log(num1 - num2); // Substract Result: 5
+console.log(num1 * num2); // Multiply Result: 50
+console.log(num1 / num2); // Divide Result: 2
+console.log(num1 % num2); // Module Resukt: 0
+
+console.log(num1); // Result: 10
+console.log(num1++); // Result: 10
+console.log(num1); // Result: 11
+
+console.log(num1); // Result: 11
+console.log(--num1); // Result: 10
+console.log(num1); // Result: 10
+
+console.log("Hello " + name); // Result: Hello Alejandro
