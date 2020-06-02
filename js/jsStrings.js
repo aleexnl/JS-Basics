7f004f483e9613438c66385a94da021eba9bf6ee
Alejandro Nieto Luque <aleexnl@MacBook-Pro.local>
2020-06-02 16:28:44 +0200

    Updated .gitignore and strings

--- js/jsStrings.js
+++ js/jsStrings.js
@@ -23,26 +23,18 @@ console.log("Position of Bye: " + msg.indexOf("Bye")); // Result: -1
 // replace(search, replace) -> Replaces the searched string for the specified in the replace argument.
 console.log("Replaced text: " + msg.replace("World", "Team")); // Result: Hello Team!
 
-//substring(start [,end]) -> Substracts the characters from the position to the end if not especified.
+// substring(start [,end]) -> Substracts the characters from the position to the end if not especified.
 console.log("Substracted text with no end" + msg.substring(1)); // Result: ello World!
 console.log("Substracted text with end" + msg.substring(1, 8)); // Result: ello Wo
 
-/*
-slice(inicio [,fin]) -> Igual que substring pero admite valores negativos, si ponemos valores negativos empezará desde atrás
-Si no se incluye el final extrae hasta el final
-	(2,-4) -> Empieza a contar en el tercer caracter y los 4 últimos no los considera
-*/
+// slice(start [,end]) -> Same as substring. Slice allows negative values, so it will start backwards if one is specified.
+console.log(msg.slice(-3));
+console.log(msg.slice(2));
+console.log(msg.slice(0, -2));
 
-/*console.log(cadena.slice(-3));
-console.log(cadena.slice(2));
-console.log(cadena.slice(0,-2));*/
-
-/*
-trim()-> Elimina los espacios al inicio y al final de la cadena
-*/
-
-/*let cadena2 = 'Hola youtube, estamos trabajando con cadenas';
-console.log(cadena2.trim());*/
+// trim()-> Elimina los espacios al inicio y al final de la cadena
+let welcome = "Hello world! We are working with strings";
+console.log(welcome.trim());
 
 /*
 --ES6--
