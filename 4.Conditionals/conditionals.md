81f50f324415a2aea53ae8f08af7dcac94b45375
aleexnl <alex.nieto0027@gmail.com>
2020-07-03 12:06:13 +0200

    Added conditionals in javascript

---
+++ 4.Conditionals/conditionals.md
@@ -0,0 +1,53 @@
+# JavaScript Conditionals
+
+## 1. Introduction
+
+Conditional statements control behavior in JavaScript and determine whether or not pieces of code can run.
+
+There are multiple different types of conditionals in JavaScript including:
+
+- “If” statements: where if a condition is true it is used to specify execution for a block of code.
+- “Else” statements: where if the same condition is false it specifies the execution for a block of code.
+- “Else if” statements: this specifies a new test if the first condition is false.
+
+## 2. If Statement
+
+As the most common type of conditional, the if statement only runs if the condition enclosed in parentheses () is truthy.
+
+![alt text](../assets/img/conditionals/js-if-statement.png)
+
+```js
+if (10 > 5) {
+  console.log("if block"); // Output: if block
+}
+```
+
+## 3. Else Statement
+
+You can extend an if statement with an else statement, which adds another block to run when the if conditional doesn’t pass.
+
+![alt text](../assets/img/conditionals/js-if-else-statement.png)
+
+```js
+if (10 < 5) {
+  console.log("if block");
+} else {
+  console.log("else block"); // Output: else block
+}
+```
+
+## 4. Else if Statement
+
+You can also extend an if statement with an else if statement, which adds another conditional with its own block.
+
+![alt text](../assets/img/conditionals/js-if-else-if-statement.png)
+
+```js
+if (10 < 5) {
+  console.log("if block");
+} else if (10 > 5) {
+  console.log("else if block"); // Output: else if block
+} else {
+  console.log("else block");
+}
+```
--- 4.Conditionals/jsConditionals.js
+++ 4.Conditionals/jsConditionals.js
@@ -1,6 +1,29 @@
+/*
+░█████╗░░█████╗░███╗░░██╗██████╗░██╗████████╗██╗░█████╗░███╗░░██╗░█████╗░██╗░░░░░░██████╗
+██╔══██╗██╔══██╗████╗░██║██╔══██╗██║╚══██╔══╝██║██╔══██╗████╗░██║██╔══██╗██║░░░░░██╔════╝
+██║░░╚═╝██║░░██║██╔██╗██║██║░░██║██║░░░██║░░░██║██║░░██║██╔██╗██║███████║██║░░░░░╚█████╗░
+██║░░██╗██║░░██║██║╚████║██║░░██║██║░░░██║░░░██║██║░░██║██║╚████║██╔══██║██║░░░░░░╚═══██╗
+╚█████╔╝╚█████╔╝██║░╚███║██████╔╝██║░░░██║░░░██║╚█████╔╝██║░╚███║██║░░██║███████╗██████╔╝
+░╚════╝░░╚════╝░╚═╝░░╚══╝╚═════╝░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝╚═════╝░
+Control behavior in JavaScript and determine whether or not pieces of code can run.
+*/
+
 let isOn = true;
 let isPlugged = true;
 
+// Simple conditional
+if (isOn) {
+  console.log("Lamp is turned on.");
+}
+
+// Compound conditional
+if (isOn) {
+  console.log("Lamp is turned on.");
+} else {
+  console.log("Lamp is turned off.");
+}
+
+// Multiple conditional
 if (isOn && isPlugged) {
   console.log("Lamp is turned on.");
 } else if (isOn && !isPlugged) {
@@ -9,23 +32,30 @@ if (isOn && isPlugged) {
   console.log("Lamp is turned off.");
 }
 
-// Order numbers
+/* Exercice: Permutation 3 numbers
+Order greater to lower number with a conditional.
+ */
 let a = 5;
 let b = 8;
 let c = 90;
 
+// Check if a is the greatest number
 if (a >= b && a >= c) {
+  // Check if b is greater than c
   if (b >= c) {
     console.log(`${a}, ${b}, ${c}`);
+    // If not, we got the right order
   } else {
     console.log(`${a}, ${c}, ${b}`);
   }
+  // Check if b is the greatest number
 } else if (b >= a && b >= c) {
   if (a >= c) {
     console.log(`${b}, ${a}, ${c}`);
   } else {
     console.log(`${b}, ${c}, ${a}`);
   }
+  // Check if c is the greatest number
 } else if (c >= a && c >= b) {
   if (a >= b) {
     console.log(`${c}, ${a}, ${b}`);
