81dce2890e311527babe362a0c08746088479119
aleexnl <alex.nieto0027@gmail.com>
2020-07-16 10:45:51 +0200

    Exercice 1

--- Exercices/Chapter 1 to 6/jsExercices.js
+++ Exercices/Chapter 1 to 6/jsExercices.js
@@ -3,18 +3,37 @@ function exerciceOne() {
   let name = prompt("Input your name:");
   let lastname = prompt("Input your lastname:");
   let age = prompt("Input your age:");
+  let msg; // Variable to build the message
 
-  console.log(`Hello ${name} ${lastname}.`);
-
-  // If we can't retrieve a int from the string, we won't show the age.
-  if (isNaN(parseInt(age))) {
-    // parseInt allows you to get a integer from a string.
-    console.log("I do not know your age.");
+  if (typeof name == "string" && name.length > 0) {
+    msg = `Hello ${name}`;
   } else {
-    console.log(
-      ` You are ${age} years old. The next year you will be ${
-        parseInt(age) + 1
-      } years old.`
-    );
+    msg = `Hello Anonymous`;
   }
+
+  if (typeof lastname == "string" && lastname.length > 0) {
+    msg += ` ${lastname}`;
+  }
+
+  if (isNaN(parseInt(age))) {
+    msg += ". I do not know your age.";
+  } else {
+    msg += ` You are ${age} years old. The next year you will be ${
+      parseInt(age) + 1
+    } years old.`;
+  }
+
+  console.log(msg);
+}
+
+function exerciceOneSimple() {
+  let name = prompt("Input your name:");
+  let lastname = prompt("Input your lastname:");
+  let age = prompt("Input your age:");
+
+  console.log(
+    `Hello ${name} ${lastname}. You are ${age} years old and the next year you will be ${
+      parseInt(age) + 1
+    } years old.`
+  );
 }
