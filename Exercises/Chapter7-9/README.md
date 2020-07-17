c4af96ae422a7861768b1c9f70482500bdae3d6c
aleexnl <alex.nieto0027@gmail.com>
2020-07-17 14:43:42 +0200

    Added & Updated Exercices

---
+++ Exercises/Chapter7-9/README.md
@@ -0,0 +1,12 @@
+1. Create a Book class and do the following steps:
+
+   - Add the properties: title, author, year and genre.
+   - Create a method that returns all the information in the book.
+   - Order 3 books and store them in an array
+   - The books will be inserted when starting the program requesting the data with a prompt.
+   - Validate that the fields are not entered empty
+   - Validate that the year is a number and that it has 4 digits
+   - Validate that the genre is: adventure, terror or fantasy
+   - Create a function that displays all the books
+   - Create a function that shows the authors in alphabetical order
+   - Create a function that asks for a genre and displays the information of the books that belong to that genre using a method that returns the information
---
+++ Exercises/Chapter7-9/jsExercises.js
@@ -0,0 +1,100 @@
+/*
+- Create a function that asks for a genre and displays the information of 
+    the books that belong to that genre using a method that returns the information */
+
+class Book {
+  constructor(title, author, year, genre) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.genre = genre;
+  }
+
+  getBook() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Genre: ${this.genre}`;
+  }
+}
+
+const askGenre = () => {
+  // Ask book genre
+  let genre;
+  const genreList = ["ADVENTURE", "TERROR", "FANTASY"];
+  console.log("GENRES: Adventure, Terror, Fantasy");
+  do {
+    genre = prompt("Write the genre of the book");
+    console.log();
+  } while (
+    !genre ||
+    genre.length <= 0 ||
+    genreList.indexOf(genre.toUpperCase()) == -1
+  );
+  return genre;
+};
+
+const askTitle = () => {
+  // Ask book title
+  let bookTitle;
+  do {
+    bookTitle = prompt("Write the book title.");
+  } while (!bookTitle || bookTitle.length <= 0);
+  return bookTitle;
+};
+
+const askAuthor = () => {
+  // Ask book author
+  let author;
+  do {
+    author = prompt("Write the author of the book.");
+  } while (!author || author.length <= 0);
+  return author;
+};
+
+const askYear = () => {
+  // Ask book year
+  let year;
+  do {
+    year = parseInt(prompt("Write the year of the publication (YYYY)."));
+  } while (!year || isNaN(year) || year <= 999 || year > 9999);
+  return year;
+};
+
+const createBook = () => {
+  // Create book object using the askXX() functions
+  let title = askTitle();
+  let author = askAuthor();
+  let year = askYear();
+  let genre = askGenre();
+  return (book = new Book(title, author, year, genre));
+};
+
+const createBooks = () => {
+  // Create 3 books
+  let books = [];
+  for (let index = 1; index <= 3; index++) {
+    books.push(createBook());
+  }
+  return books;
+};
+
+const showBooks = (books) => {
+  // Show all books information inside books array
+  for (const book of books) {
+    console.log(`${book.title} Book information:`);
+    console.log(
+      `Title: ${book.title}\nAuthor: ${book.author}\nYear: ${book.year}\nGenre: ${book.genre}`
+    );
+  }
+};
+
+const showSortedAuthors = (books) => {
+  // Show sorted authors in the books array
+  let authors = [];
+  for (const book of books) {
+    authors.push(book.author);
+  }
+  return `Authors alphabetically sorted: ${authors.sort()}`;
+};
+
+let books = createBooks(); // create books
+showBooks(books); // show books
+showAuthors(books); // show sorted authors
